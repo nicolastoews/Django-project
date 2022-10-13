e950fb86caecc50c4afc8003c01e0ef57133ebec
Nicolas Toews <toewsnicolas@gmail.com>
2022-10-13 10:37:42 -0300

    Read changelog.txt

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     edad = models.IntegerField()
-    fecha_nacimiento = models.DateTimeField(null=True)
+    fecha_creacion = models.DateTimeField(null=True)
     
-    def __str__(self):
-        return f'{self.nombre} {self.apellido}' #para que en django admin nos aparezca cada objeto con el nombre y apellido de la persona que es dicho objeto
+def __str__(self):
+    return f'{self.nombre} {self.apellido}' #para que en django admin nos aparezca cada objeto con el nombre y apellido de la persona que es dicho objeto
